befe555e51ded7e67215c2bd7aae1285fb03dc71
stslex <ilya977.077@gmail.com>
2024-07-19 21:38:01 +0300

    test route

--- src/routes/user/mod.rs
+++ src/routes/user/mod.rs
@@ -9,11 +9,17 @@ pub trait UserRouter {
 impl UserRouter for Rocket<Build> {
     fn mount_user_route<'a>(self, base_url: &'a str) -> Self {
         let path = format!("{}/user", base_url);
-        self.mount(path, routes![get_user])
+        self.mount(path, routes![get_user, get_user_test_name])
     }
 }
 
 #[get("/user_name")]
 async fn get_user() -> ApiResponse<'static, &'static str> {
     ApiResponse::Ok("user")
+}
+
+#[get("/user_name/<name>")]
+async fn get_user_test_name(name: String) -> &'static str {
+    let formatted_hello = format!("{}{}", "hello ".to_owned(), name);
+    Box::leak(formatted_hello.to_string().into_boxed_str())
 }
